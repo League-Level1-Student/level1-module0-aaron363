b832f1c7ec221d3986f2ff7e7deb300fa173dd35
student <student@ip-172-31-62-113.us-west-2.compute.internal>
2021-04-17 12:54:02 -0700

    Level 1 Module 0

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,12 +1,44 @@
 package _07_fortune_cookie;
 
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class FortuneCookie {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class FortuneCookie implements ActionListener {
 
 	public static void main(String[] args) {
+		
+		new FortuneCookie().showButton();
+	}
+	public void showButton() {
 		JFrame frame = new JFrame();
-
+		frame.setVisible(true);
+		
+		JButton button = new JButton();
+	    frame.add(button);
+	    frame.pack();
+	    
+	    button.addActionListener(this);
+	    
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		int rand = new Random().nextInt(5);
+		if(rand == 0) {
+			JOptionPane.showMessageDialog(null, "Do a good deed today.");
+		}else if(rand == 1) {
+			JOptionPane.showMessageDialog(null, "Be careful on Tuesday.");
+		}else if(rand == 2) {
+			JOptionPane.showMessageDialog(null, "Success is a journey, not a destination.");
+		}else if(rand == 3) {
+			JOptionPane.showMessageDialog(null, "The harder you work, the luckier you get.");
+		}else {
+			JOptionPane.showMessageDialog(null, "Pick battles big enough to matter, small enough to win.");
+		}
 	}
 
 }
